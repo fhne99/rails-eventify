c462cdb5a739a81978529b83c906c6e669c74988
Honorine Faure gh api ...honorinefaure@gmail.com <honorinefaure@gmail.com>
2024-05-16 20:59:51 +0200

    event has at least one price validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :prices
   has_one_attached :photo
   validates :title, :description, :location, :start_date, presence: true
   validates :title, length: { maximum: 100 }
   validates :description, length: { maximum: 1000 }
   validates :available_tickets, numericality: { greater_than_or_equal_to: 0 }
-#  validates :start_date, :end_date, timeliness: { type: :datetime }
+  # validates :start_date, :end_date, timeliness: { type: :datetime }
 
   # Catégories d'évènements prédéfinies disponibles
   CATEGORIES = ["Concert", "Festival", "Spectacle", "Sport", "Art et culture", "Conférence", "Cours et atelier"]
@@ -14,7 +13,13 @@ class Event < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 
-  #def end_date_after_start_date
-  #  errors.add(:end_date, "Doit être après l'heure et la date de début définies") if end_date <= start_date
-  #end
+  # Un event doit avoir au moins un prix
+  has_many :prices
+  validate :at_least_one_price_present
+
+  private
+
+  def at_least_one_price_present
+    errors.add(:base, "At least one price must be present") if prices.none?
+  end
 end
